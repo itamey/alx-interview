bd2fa49e4ff25b3538eb9fd63cd12e90228686ee
eyoshalom <eyoshalom@yahoo.com>
2023-09-23 04:52:28 -0700

    make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""
-Change comes from within
-"""
+"""Making change O(n)"""
 
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet a given amount total
-    """
-    if total <= 0:
+    """Clasic Bottom-Up dynamic programming"""
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
         return 0
-    dp = [0] + [float("inf")] * (total)
+
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[-1] if dp[-1] != float("inf") else -1
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    return temp_value if total == 0 else -1
+
+
+if __name__ == '__main__':
+
+    print(makeChange([1, 2, 25], 37))
+
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
